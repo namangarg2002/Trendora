a8bd37f236a0ec030b1bdb91a62710bc2b5d9f14
namangarg2002 <namangarg20021128@gmail.com>
2025-03-09 18:09:38 +0530

    updated changes

--- trendora/src/App.js
+++ trendora/src/App.js
@@ -1,10 +1,29 @@
-import './App.css';
-
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect, useState } from 'react';
+import Banner from './Components/Banner/Banner';
+import Navbar from './Components/Navbar/Navbar';
 function App() {
+  const [orderPopup, setOrderPopup] = useState(false);
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  }
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
-    <div className='bg-slate-500'>
-      Hello
-    </div>
+    <>
+      <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
+        <Navbar handleOrderPopup={handleOrderPopup}/>
+        <Banner/>
+      </div>
+    </>
   );
 }
 
--- trendora/tailwind.config.js
+++ trendora/tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4263eb',
+        secondary: '#63e6be'
+      },
+      container:{
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
   },
+  darkMode: "selector",
   plugins: [],
 }
 
